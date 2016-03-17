2d7834687abc4eb2eb1ee090f9aaf5870466f951
blackbbc <505968815@qq.com>
2016-03-17 22:34:51 +0000

    Define socket staus

--- Server/src/main/java/Settings.java
+++ Server/src/main/java/Settings.java
@@ -2,7 +2,15 @@
  * Created by sweet on 3/17/16.
  */
 public class Settings {
+    /*
+     * socket状态
+     * LOGOUT--已登出
+     * LOGIN--已登陆
+     * RELOGIN--已发送100条消息，需重新登陆
+     * IGNORE--超过5条/秒的限制，忽略
+     *
+     */
     public static enum Status {
-        LOGIN, LOGOUT, NEED_RELOIN
+        LOGIN, LOGOUT, NEED_RELOGIN
     };
 }
