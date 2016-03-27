6b36f78b2eed5fdbc84f3e5f88c461a5c80122f5
Kris_Chan <743628145@qq.com>
2016-03-27 22:48:06 +0800

    1.server unit test;

---
+++ Server/test/NIOClientTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kris Chan on 10:37 PM 3/27/16 .
+ * All right reserved.
+ */
+public class NIOClientTest {
+
+    @Test
+    public void testOnConnect() throws Exception {
+
+    }
+
+    @Test
+    public void testOnLogin() throws Exception {
+
+    }
+
+    @Test
+    public void testOnRegister() throws Exception {
+
+    }
+
+    @Test
+    public void testOnRelogin() throws Exception {
+
+    }
+
+    @Test
+    public void testOnSend() throws Exception {
+
+    }
+
+    @Test
+    public void testOnDisconnect() throws Exception {
+
+    }
+
+    @Test
+    public void testOnError() throws Exception {
+
+    }
+}
