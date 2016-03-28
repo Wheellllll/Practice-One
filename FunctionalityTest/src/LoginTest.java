a780d280dabcbbb97125411129c074f8964db997
WishSummer <summerdaynevergone@gmail.com>
2016-03-28 10:21:04 +0800

    no message

--- FunctionalityTest/src/LoginTest.java
+++ FunctionalityTest/src/LoginTest.java
@@ -1,9 +1,32 @@
+import utils.MessageBuilder;
+
 /**
  * Created by summer on 3/27/16.
  */
 public class LoginTest {
    public static void Main(String args[])
    {
+       Server server = new Server();
+       Client client = new Client();
+
+       //注册测试账号
+       MessageBuilder msgBuilder = new MessageBuilder();
+       msgBuilder.add("event","reg");
+       msgBuilder.add("username","funcTest");
+       msgBuilder.add("password","123456");
+       String msg = msgBuilder.build();
+       client.sendMessage(msg);
+
+       //登录
+       MessageBuilder loginMsgBuilder = new MessageBuilder();
+       loginMsgBuilder.add("event","login");
+       loginMsgBuilder.add("username","funcTest");
+       loginMsgBuilder.add("password","123456");
+       String loginMsg = loginMsgBuilder.build();
+       client.sendMessage(loginMsg);
+
+       
+
 
    }
 
