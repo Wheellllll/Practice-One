38de96f278c3f85822317f71d9790363adf32bae
blackbbc <505968815@qq.com>
2016-03-29 08:17:33 +0800

    Now we can send multiline message

--- Client/src/main/java/ui/ChatRoomForm.java
+++ Client/src/main/java/ui/ChatRoomForm.java
@@ -2,9 +2,7 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 /**
  * Provide a GUI of the chat rom for the client. Users use this GUI to interact with the server.
@@ -12,11 +10,11 @@ import java.awt.event.WindowEvent;
 public class ChatRoomForm {
     private JFrame frame = new JFrame("最简单的聊天室");
     private JTextArea messageArea = new JTextArea();
-    private JTextField chatArea = new JTextField();
+    private JTextArea chatArea = new JTextArea(3, 30);
     private JButton sendBtn = new JButton("发送");
 
     public ChatRoomForm() {
-        frame.setSize(500, 300);
+        frame.setSize(500, 500);
         initFrame();
         frame.setVisible(true);
     }
@@ -27,20 +25,21 @@ public class ChatRoomForm {
         container.setLayout(new BorderLayout());
         JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout(new BorderLayout());
-        bottomPanel.add(chatArea, BorderLayout.CENTER);
+        JScrollPane chatScrollPane = new JScrollPane(chatArea);
+        bottomPanel.add(chatScrollPane, BorderLayout.CENTER);
         bottomPanel.add(sendBtn, BorderLayout.EAST);
 
         //顶部消息区域
-        JScrollPane scrollPane = new JScrollPane(messageArea);
-        scrollPane.setHorizontalScrollBarPolicy(
+        JScrollPane messageScrollPane = new JScrollPane(messageArea);
+        messageScrollPane.setHorizontalScrollBarPolicy(
                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
         );
-        scrollPane.setVerticalScrollBarPolicy(
+        messageScrollPane.setVerticalScrollBarPolicy(
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
         );
         messageArea.setEditable(false);
 
-        container.add(scrollPane, BorderLayout.CENTER);
+        container.add(messageScrollPane, BorderLayout.CENTER);
         container.add(bottomPanel, BorderLayout.SOUTH);
 
         frame.add(container);
@@ -84,8 +83,15 @@ public class ChatRoomForm {
      * Add a listener to the send event
      * @param listener listener called when send message
      */
-    public void setOnSendListener(ActionListener listener) {
+    public void setOnSendListener(final ActionListener listener) {
         sendBtn.addActionListener(listener);
-        chatArea.addActionListener(listener);
+        chatArea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                int code = keyEvent.getKeyCode();
+                int modifiers = keyEvent.getModifiers();
+                if (code == KeyEvent.VK_ENTER && modifiers == KeyEvent.CTRL_MASK) listener.actionPerformed(null);
+            }
+        });
     }
 }
